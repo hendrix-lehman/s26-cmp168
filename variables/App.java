4a0f5b7d281f40858ea763934b5f9465ee340837
Hendrix Tavarez <htavarez@gmail.com>
2026-01-27 19:39:59 -0500

    variables

---
+++ variables/App.java
@@ -0,0 +1,12 @@
+// add package name here if needed
+//
+class App {
+
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    System.out.println("Sum: " + (a + b));
+    
+  }
+}
+
