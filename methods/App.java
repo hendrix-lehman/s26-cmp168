4de7062dd1a2986ff06b2adb1f165932002c19a3
Hendrix Tavarez <htavarez@gmail.com>
2026-02-10 19:49:53 -0500

    feb 10

--- methods/App.java
+++ methods/App.java
@@ -13,6 +13,7 @@ class App {
   // all "functions" must be methods
   //
 
+  // this is a method that returns a double
   public double getTargetNumber() { // this method is part of the App class, and can only be used if App is instantiated
     // randomly generate a number, multiply it by 7 and divide by 2 and add 1 to it.
     // that's going to be our target number
@@ -22,6 +23,8 @@ class App {
 
   public static void main(String[] args) {
 
+    House.ringDoorbell("Bob"); // call the static ringDoorbell method on the House class, this method does not require an instance of the House class to be called
+
     // randomly generate a number, multiply it by 7 and divide by 2 and add 1 to it.
     // that's going to be our target number
 
@@ -30,8 +33,17 @@ class App {
     double target = app.getTargetNumber();
     System.out.println("Target number: " + target);
 
-    House myHouse = new House(); // create an instance of the House class
+    House myHouse = new House(); // create an instance of the House class, we call this an object of the House class
+    myHouse.downloadTheInternet(); // call the downloadTheInternet method on the House instance, this is not a good idea, it would be resource intensive and time intensive
     myHouse.ringDoorbell(); // call the ringDoorbell method on the House instance
+
+    House redHouse = new House("red"); // create another instance of the House class, this time we use the overloaded constructor to set the color to red
+    redHouse.ringDoorbell(); // call the ringDoorbell method on the redHouse instance
+
+    House greenHouse = new House("green", "456 Oak St"); // create another instance of the House class, this time we use the overloaded constructor to set the color to green and address to 456 Oak St
+    House.ringDoorbell("Alice"); // call the static ringDoorbell method on the House class, this method does not require an instance of the House class to be called
+    // not accessible here
+    // greenHouse.exchangeKeys(); // call the exchangeKeys method on the greenHouse instance, this is not a good idea, it would be resource intensive and time intensive
    
   }
 
--- methods/House.java
+++ methods/House.java
@@ -1,11 +1,83 @@
 
 
+// class House describes a house.
+// class is use to describe a thing
 class House {
-  public void ringDoorbell() {
-    System.out.println("Ding Dong!");
+  // member variables, fields, attributes, properties,
+  // whatever you want to call them, they are all the same.
+  // they are use to store information about the object
+  //
+  // member variables can be thought as "data" that describes the object.
+  // so, the "state" of the object is described by its member variables.
+  private String address;
+
+  private boolean hasGarage;
+
+  private String color = "blue";
+  // by initializing the color variable,
+  // we are guaranteeing that every House object is blue by default
+
+  // constructors
+  // are use to initialize the object when it is created
+  // these are special methods that are called when an object is created.
+  //
+  // your code should be a simple as possible
+  // not resource intensive, and not time intensive.
+  public House() {
+    // this is the default constructor, it takes no arguments
+    // and it initializes the member variables to default values
+    this.address = "123 Main St";
+    this.hasGarage = false;
+  } 
+
+  // overloaded constructor, this one takes the color as an argument
+  public House(String color) {
+    // this constructor takes a color as an argument and initializes the color member variable
+    this.color = color;
   }
 
+  public House(String color, String address) {
+    // this constructor takes the color and address as arguments and initializes the color and address member variables
+    this.color = color;
+    this.address = address;
+  }
+  
   private class Room {
     // Room class implementation
+  }
+
+  // this is a method
+  // methods are use to define the behavior of the object
+  // they have a return type, a name, and a body
+  // in this case the return type is void,
+  // meaning that this method does not return anything
+  //
+  // methods can be overloaded as well
+  //
+  // by convention,
+  // methods names should start with a lowercase letter
+  // and if using multiple words, each word should start 
+  // with an uppercase letter
+  public void ringDoorbell() {
+    System.out.println("Ding Dong! Someone is at the door!");
+  }
+
+  // this is a static method, it belongs to the class rather than
+  // to any particular instance of the class, it can be called without creating an instance of the class
+  public static void ringDoorbell(String name) {
+    System.out.println("Ding Dong! " + name + " is at the door!");
+  }
+
+  public void downloadTheInternet() {
+    // this method is not a good idea, it would be resource intensive and time intensive
+    // we should avoid writing code like this
+    //
+    //connect to some Internet service provider to exchange keys
+    exchangeKeys();
+  }
+
+  private void exchangeKeys() {
+    // this method is not a good idea, it would be resource intensive and time intensive
+    // we should avoid writing code like this
   }
 }
